f915b1771efdce835905407bea7f64f5bf3760ca
VCX <88580298+Viablecobra@users.noreply.github.com>
2025-10-23 14:54:46 +0530

    Add files via upload

---
+++ build.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("cargo:rerun-if-changed=src/lib.rs");
+    println!("cargo:rerun-if-changed=src/android.rs");
+    
+    // Set soname for shared library
+    if std::env::var("TARGET").unwrap().contains("linux") {
+        println!("cargo:rustc-cdylib-link-arg=-Wl,-soname,libnoflipbookanim.so");
+    }
+    
+    // Android specific linking
+    if std::env::var("TARGET").unwrap().contains("android") {
+        println!("cargo:rustc-link-lib=log");
+        println!("cargo:rustc-link-lib=android");
+    }
+}
